2506312f8f8311e54ac44dc54aed948366e450fe
IngvaldssonMattias <mattias.ingvaldsson@chasacademy.se>
2026-03-23 15:18:35 +0100

    fixed SafeAreaProvider, SafeAreaView

--- linkopingReactNative/src/components/Timer/Timer.tsx
+++ linkopingReactNative/src/components/Timer/Timer.tsx
@@ -1,58 +1,60 @@
 import React from "react";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, SafeAreaView } from "react-native";
 import { useSimpleTimer } from "../../utils/useSimpleTimer";
 import { formatTime } from "../../utils/Time";
 import { Styles } from "../../styles/Timer.style";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Timer = () => {
-    const { timeLeft, mode, isRunning, setIsRunning, reset } = useSimpleTimer()
+  const { timeLeft, mode, isRunning, setIsRunning, reset } = useSimpleTimer();
 
-    return (
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
         <View style={Styles.container}>
-        <View style={Styles.circle}>
+          <View style={Styles.circle}>
             <Text style={Styles.mode}>{mode}</Text>
             <Text style={Styles.time}>{formatTime(timeLeft)}</Text>
-        </View>
+          </View>
 
-
-        <View style={Styles.buttons}>
+          <View style={Styles.buttons}>
             <Pressable
-            onPress={() => setIsRunning(true)}
-            style={({ pressed }) => [
+              onPress={() => setIsRunning(true)}
+              style={({ pressed }) => [
                 Styles.button,
                 Styles.startButton,
                 pressed && Styles.buttonPressed,
-            ]}
+              ]}
             >
-                <Text style={Styles.buttonText}>Start</Text>
+              <Text style={Styles.buttonText}>Start</Text>
             </Pressable>
 
-
             <Pressable
-            onPress={() => setIsRunning(false)}
-            style={({ pressed }) => [
+              onPress={() => setIsRunning(false)}
+              style={({ pressed }) => [
                 Styles.button,
                 Styles.stopButton,
                 pressed && Styles.buttonPressed,
-            ]}
+              ]}
             >
-                <Text style={Styles.buttonText}>Stop</Text>
+              <Text style={Styles.buttonText}>Stop</Text>
             </Pressable>
 
-
             <Pressable
-            onPress={reset}
-            style={({ pressed }) => [
+              onPress={reset}
+              style={({ pressed }) => [
                 Styles.button,
                 Styles.resetButton,
                 pressed && Styles.buttonPressed,
-            ]}
+              ]}
             >
-                <Text style={Styles.buttonText}>Reset</Text>
+              <Text style={Styles.buttonText}>Reset</Text>
             </Pressable>
+          </View>
         </View>
-        </View>
-    );
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 export default Timer;
